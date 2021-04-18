3b91f1f4a471f882b50bf3cfdf7b378167e56d50
ZeeLog <Danjel.Zammit@gmail.com>
2021-04-18 17:48:30 +0000

    msm8937-common: Kang DRM widevine 1.3 vintf fragment from lmi

--- msm8937-common/msm8937-common-vendor.mk
+++ msm8937-common/msm8937-common-vendor.mk
@@ -756,4 +756,5 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.callinfo-V1.0-java \
     vendor.qti.ims.rcsconfig-V1.0-java \
     android.hardware.gnss@2.1-service-qti \
+    manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service
